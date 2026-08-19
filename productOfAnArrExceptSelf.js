6306109d34505c9eaa554db0c8742dbf8f84a451
UR Rehman <umair.rehman221@gmail.com>
2026-08-19 18:07:42 +0400

    Optimize space complexity for product of array
    Refactored productOfAnArrExceptSelf function to optimize space complexity to O(1) by eliminating prefix and suffix arrays.

--- productOfAnArrExceptSelf.js
+++ productOfAnArrExceptSelf.js
@@ -20,7 +20,53 @@ for(let i =0; i<nums.length; i++){
 
 // OPTIMIZED APPROACH 
 
-let numbers = [1, 2, 3, 4];
-let answer = 1;
+const number = [1, 2, 3, 4];
+const answer = new Array(n);  // this will create n size array
+const prefix = new Array(n).fill(1);
+let suffix= new Array(n).fill(1);    // suffix = [1, 1, 1, 1]
+const n = number.length;
 // preffix
-for(){}
+for(let i=1; i<n; i++){
+  prefix[i] = prefix[i-1] * number[i-1];    // first loop prefix[1, 1]  2ndloop prefix[1,1,2,6];
+};
+
+for(let i=n-2; i>=0; i--){
+  suffix[i] = suffix[i+1] * number[i+1];  // firstloop [1]   2ndloop suffix[, 4, 1]   3rdloop sfix[]
+}
+
+for(let i=0; i<n; i++){
+  answer[i] = prefix[i] * suffix[i];  
+}
+
+// Prefix loop   → O(n)
+// Suffix loop   → O(n)
+// Answer loop   → O(n)
+
+// Total         → O(3n)
+//               → O(n)
+
+// SPACE COMPLEXITY
+// prefix  → O(n)
+// suffix  → O(n)
+// answer  → O(n)      
+                  //  i neeed SAPCE COMPEXITY O(1)
+
+
+const number = [1, 2, 3, 4];
+const answer = new Array(n);  // this will create n size array
+answer[0] = 1;
+const n = number.length;
+// preffix
+
+for(let i=1; i<n; i++){
+  answer[i] = answer[i-1] * number[i-1];    // first loop prefix[1, 1]  2ndloop prefix[1,1,2,6];
+};
+
+let suffix = 1;
+for(let i=n-1; i>=0; i--){
+  answer[i] *= suffix;  // firstloop [1]   2ndloop suffix[, 4, 1]   3rdloop sfix[]
+  suffix *= number[i]
+}
+
+here the SPACE COMPLEXITY IS O(1);
+
