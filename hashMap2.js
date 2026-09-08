1412d5d98a05e0d8f7a5d7eb450cc77651f938e2
UR Rehman <umair.rehman221@gmail.com>
2026-09-08 19:00:39 +0400

    Implement longest streak function with sorting
    Added O(n log n) approach to find the longest streak in an array.

--- hashMap2.js
+++ hashMap2.js
@@ -29,3 +29,32 @@ function(nums) {
 
     return longest;
 };
+
+
+O(n log n) approach with sort 
+function longest(arr){
+let n = arr.length;
+if(n === 0) return 0;
+
+let streak = 1;
+let longest = 1;
+let sortedArr = arr.sort((a, b) => a - b);
+for(let i=1; i<n; i++){
+if(sortedArr[i] === sortedArr[i - 1]){
+  continue;
+} 
+
+if(sortedArr[i] === sortedArr[i -1] + 1){
+  streak++;
+} 
+
+else {
+ streak =1;
+}
+longest = Math.max(longest, streak);
+
+}
+return longest;
+}
+
+
