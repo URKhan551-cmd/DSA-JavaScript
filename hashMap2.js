be4a8bc1d85df63ccaa51c579d8f8a467adfd041
UR Rehman <umair.rehman221@gmail.com>
2026-09-09 18:01:28 +0400

    Implement optimized merge sort for inversion count

--- hashMap2.js
+++ hashMap2.js
@@ -433,4 +433,57 @@ return count;
 }
 
 
-    
+    // OPTIMIZED APPROACH 
+
+function inversion(arr){
+
+
+function mergeSort(arr){
+  if(arr.length <= 1){
+  return {
+   sorted: arr,
+   count: 0
+}
+  }
+let mid = Math.floor(arr.length / 2);
+let left = mergeSort(arr.slice(0, mid));
+let right = mergeSort(arr.slice(mid));
+
+const merged = merge(left.sorted, right.sorted);
+return {
+ sorted: merged.result,
+ count: left.count + right.count + merged.count
+}
+}
+
+function merge(left, right){
+ let result =[];
+let i = 0;
+let j = 0;
+let count = 0;
+
+while(i < left.length && j< right.length){
+  if(left[i] <= right[j]){
+   result.push(left[i]);
+   i++;
+} else {
+  result.push(right[j]);
+ count += left.length - i;
+j++;
+ }
+}
+
+while(i < left.length){
+  result.push(left[i]);
+ i++;
+}
+while(j < right.length){
+ result.push(right[j]);
+j++;
+}
+
+return {result, count};
+}
+
+return mergeSort(arr).count;
+}
