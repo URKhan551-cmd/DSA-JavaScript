87d0c30d0c943c12c04daa2ad53fea118f98eaa2
UR Rehman <umair.rehman221@gmail.com>
2026-09-10 19:32:22 +0400

    Add findMaxAverage function for max average subarray
    Implement sliding window algorithm to find maximum average subarray.

--- slidingWindow.js
+++ slidingWindow.js
@@ -2,7 +2,22 @@ Note for LeetCode #643
 LeetCode #643 is actually Maximum Average Subarray I, not max sum.
 The logic is almost identical, but at the end you divide by k.
 
-   
+var findMaxAverage = function(nums, k) {
+    let sum = 0;
+
+    for (let i = 0; i < k; i++) {
+        sum += nums[i];
+    }
+
+    let maxSum = sum;
+
+    for (let i = k; i < nums.length; i++) {
+        sum += nums[i] - nums[i - k];
+        maxSum = Math.max(maxSum, sum);
+    }
+
+    return maxSum / k;
+};   
 
 
 
