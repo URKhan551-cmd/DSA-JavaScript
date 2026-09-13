a085b44422d226d19411b90aa88f7cb4a107719e
UR Rehman <umair.rehman221@gmail.com>
2026-09-13 18:53:47 +0400

    Add sliding window algorithm for longest substring
    Implemented a sliding window approach to find the length of the longest substring without repeating characters.

--- dynamicSlidingWind.js
+++ dynamicSlidingWind.js
@@ -19,6 +19,33 @@ for(let right=0;right<nums.length;right++){
 
 
 
+
+
+
+
+function longest(string){
+let seen = new Set();
+let maxLength =0;
+let left = 0;
+for(let right=0;right<string.length; right++){
+
+//duplicate found shrink the window.
+  while(seen.has(string[right])){
+   seen.delete(string[left]);
+left++;
+ }
+// add current character to set
+seen.add(string[right]);
+//calculate the current length
+maxLength = Math.max(maxLength, right - left + 1);
+}
+return maxLength;
+}
+
+
+
+
+
 function longest(string){
 let map = new Map();
 let maxLength =0;
