c5b656a326dc9e60e911de56d1c27b1f3d767f05
UR Rehman <umair.rehman221@gmail.com>
2026-09-13 18:49:06 +0400

    Implement longest substring length function
    Added a function to find the length of the longest substring without repeating characters.

--- dynamicSlidingWind.js
+++ dynamicSlidingWind.js
@@ -16,3 +16,20 @@ for(let right=0;right<nums.length;right++){
   // record
   best = Math.max(best, right - left + 1);
 }
+
+
+
+function longest(string){
+let map = new Map();
+let maxLength =0;
+let left = 0;
+for(let right=0;right<string.length; right++){
+  let char = string[right];
+ if(map.has(char)){
+   left = Math.max(left, map.get(char) + 1);
+}
+map.set(char, right);
+maxLength = Math.max(maxLength, right - left + 1);
+}
+return maxLength;
+}
