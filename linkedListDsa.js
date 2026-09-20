1e6a21730787ddd389a21a253f7ab80d387d7532
UR Rehman <umair.rehman221@gmail.com>
2026-09-20 18:48:41 +0400

    Add linkedReverse function to reverse linked list
    Implemented a function to reverse a singly linked list.

--- linkedListDsa.js
+++ linkedListDsa.js
@@ -17,3 +17,34 @@ let node4 = new ListNode(40);
 node1.next = node2;
 node2.next = node3;
 node3.next = node4;
+
+
+
+//********************************
+
+Reverse Linked List
+LeetCode #206
+↗
+Easy
+✓ Solved
+
+›
+details
+Flip every arrow · prev / curr / next
+Given the head of a singly linked list, reverse the list and return the new head.
+
+    function linkedReverse(head){
+ let current = head;
+let prev = null;
+
+while(current !== null){
+  let next = current.next; // current k next ko sambhal k 
+                                state m raka.
+  current.next = prev;   // usi current k next ko null banaya.
+  prev = current;         // prev pehle null ta ab humne current 
+                             //  ki value assign kiya prev= 1;
+  current = next;        // ab ye point next wale position p chala
+                           // jayega aur phir loop check hoga run hoga.
+}
+return prev;
+}
